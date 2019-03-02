da568936635cb9888d5418be432bd135b92463bb
Tobias Kleinert <tms.kleinert@gmx.de>
2019-03-02 15:48:43 +0100

    Removed data

--- Twitch_API_extension/Settings/settings.js
+++
@@ -1,8 +0,0 @@
-﻿var settings = {
-  "Command": "!followage",
-  "Permission": "everyone",
-  "Info": "",
-  "Cooldown": 1.0,
-  "Response": "Pong!",
-  "clientID": "u0qw6k8svmddooisgqjbg2sxd30xpz"
-};
